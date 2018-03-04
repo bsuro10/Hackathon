8171ba42c8ce7a62300de500bc98aa2b6bd3e144
DESKTOP-JVD2IPN\User <ronso008@gmail.com>
2018-03-04 22:45:58 +0200

    Test

--- public/javascripts/Controllers/mapController.js
+++ public/javascripts/Controllers/mapController.js
@@ -1,14 +1,14 @@
 myAppModule.controller("mapController", function($scope) {
-});
 
-function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-}
+    $scope.load = function() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+    }
+});
